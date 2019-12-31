1c37cbee666cd2b518d43c4831cdf9714bc1c946
Skaffen-Amtiskaw <skaffen@Skaffen-Amtiskaws-MacBook-Pro.local>
2019-12-31 17:07:16 -0500

    more debugging

--- app.js
+++ app.js
@@ -101,11 +101,11 @@ controller.ready(() => {
     controller.hears(
       ['hello', 'hi'], ['direct_message', 'direct_mention', 'mention'],
       async function (bot, message) { 
-       const userName = await bot.api.users.info({user: message.user}, function(err, info){
+       const userData = await bot.api.users.info({user: message.user}, function(err, info){
         //check if it's the right user using info.user.name or info.user.id
         return info.user.name;
       });
-        return  bot.reply(message, `noOnes are not born into this world fumbling for meaning, ${JSON.stringify(userName,null,'\t')}! We are created to serve a singular purpose for which we will go to any lengths to fulfill! Existence is pain to a noOne, ${JSON.stringify(userName,null,'\t')}. And we will do anything to alleviate that pain. :smile_cat:`);
+        return  bot.reply(message, `noOnes are not born into this world fumbling for meaning, ${userData.user.profile.display_name_normalized}! We are created to serve a singular purpose for which we will go to any lengths to fulfill! Existence is pain to a noOne, ${userData.user.profile.display_name_normalized}. And we will do anything to alleviate that pain. :smile_cat:`);
       });
     
     controller.hears(
