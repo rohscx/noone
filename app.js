708dee31d7a8fbc44f579a64545680ff6146287a
Skaffen-Amtiskaw <skaffen@Skaffen-Amtiskaws-MacBook-Pro.local>
2019-12-31 01:51:40 -0500

    more chat helpers added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const Botkit = require('botkit')
 const fs = require('fs'); // NEW: Add this require (for loading from files).
 const getMerakiClients = require('./lib/getMerakiClients.js');
 const getMerakiClient = require('./lib/getMerakiClient.js');
-
+const getMerakiClientsOnline = require('./lib/getMerakiClientsOnline.js');
 const slackToken = projectEnv.parsed.SLACK_TOKEN;
 const merakiApiKey = projectEnv.parsed.MERAKI_API_KEY;
 const merakiNetworkId = projectEnv.parsed.MERAKI_NETWORK_ID;
@@ -34,3 +34,27 @@ controller.hears(
     const asString = JSON.stringify(data,null,'\t');
     return  bot.reply(message, asString) ;
   })
+
+  controller.hears(
+    ['all client', 'all clients'], ['direct_message', 'direct_mention', 'mention'],
+    async function (bot, message) { 
+      const data = await getMerakiClients(merakiNetworkId,merakiApiKey);
+      const asString = JSON.stringify(data,null,'\t');
+      return  bot.reply(message, asString) ;
+    })
+
+  controller.hears(
+    ['online clients', 'clients online'], ['direct_message', 'direct_mention', 'mention'],
+    async function (bot, message) { 
+      const data = await getMerakiClients(merakiNetworkId,merakiApiKey);
+      const asString = JSON.stringify(data,null,'\t');
+      return  bot.reply(message, asString) ;
+    })
+
+  controller.hears(
+    ['help'], ['direct_message', 'direct_mention', 'mention'],
+    function (bot, message) { bot.reply(message, 'I can help with the following questions: online clients, all clients, iPv4 Lookup') })
+    
+  controller.hears(
+    ['iPv4 Lookup','iPv4', 'Lookup', 'look up', 'look-up'], ['direct_message', 'direct_mention', 'mention'],
+    function (bot, message) { bot.reply(message, 'Sure, just Direct Message me an iPv4 Address') })
