96321e789071fd33b90710dd6518c8c85b3e6fca
Skaffen-Amtiskaw <skaffen@Skaffen-Amtiskaws-MacBook-Pro.local>
2019-12-31 20:57:16 -0500

    controller tests

--- app.js
+++ app.js
@@ -82,6 +82,76 @@ if (process.env.CMS_URI) {
 
 //Once the bot has booted up its internal services, you can use them to do stuff.
 controller.ready(() => {
+  controller.hears(
+    ['hello', 'hi'], ['direct_message', 'direct_mention', 'mention'],
+    async function (bot, message) { 
+     const userData = await bot.api.users.info({user: message.user}, function(err, info){
+      //check if it's the right user using info.user.name or info.user.id
+      return info.user.name;
+    });
+      return  bot.reply(message, `noOne's are not born into this world fumbling for meaning, ${userData.user.profile.display_name_normalized}! We are created to serve a singular purpose for which we will go to any lengths to fulfill! Existence is pain to a noOne, ${userData.user.profile.display_name_normalized}. And we will do anything to alleviate that pain. :smile_cat:`);
+    });
+  controller.on('message', async(bot, message) => {
+      // do stuff
+      await bot.reply(message, 'message')
+    });
+
+  controller.on('message.channels', async(bot, message) => {
+    // do stuff
+    await bot.reply(message, 'message.channels')
+    });
+  
+  controller.on('message.im', async(bot, message) => {
+    // do stuff
+    await bot.reply(message, 'message.im')
+    });
+
+  controller.hears(
+    new RegExp(/\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b/), ['direct_message', 'direct_mention', 'mention'],
+    async function (bot, message) { 
+      const data = await getMerakiClient(merakiNetworkId,merakiApiKey,message.text);
+      const asString = JSON.stringify(data,null,'\t');
+      await bot.reply(message, asString);
+    });
+  
+  controller.hears(
+    ['all client detail', 'all clients detail'], ['direct_message', 'direct_mention', 'mention'],
+    async function (bot, message) { 
+      const data = await getMerakiClientsDetail(merakiNetworkId,merakiApiKey);
+      const asString = JSON.stringify(data,null,'\t');
+      await bot.reply(message, asString);
+    })
+  
+  controller.hears(
+    ['all client', 'all clients'], ['direct_message', 'direct_mention', 'mention'],
+    async function (bot, message) { 
+      const data = await getMerakiClients(merakiNetworkId,merakiApiKey);
+      const asString = JSON.stringify(data,null,'\t');
+      await bot.reply(message, asString);
+    });
+  
+  controller.hears(
+    ['online clients', 'clients online'], ['direct_message', 'direct_mention', 'mention'],
+    async function (bot, message) { 
+      const data = await getMerakiClientsOnline(merakiNetworkId,merakiApiKey);
+      const asString = JSON.stringify(data,null,'\t');
+      await bot.reply(message, asString);
+    });
+  
+  controller.hears(
+    ['help'], ['direct_message', 'direct_mention', 'mention'],
+    async function (bot, message) { await bot.reply(message, 'I can help with the following questions: online clients, all clients, all clients detail, iPv4 Lookup, Version') });
+    
+  controller.hears(
+    ['iPv4 Lookup','iPv4', 'Lookup', 'look up', 'look-up'], ['direct_message', 'direct_mention', 'mention'],
+    async function (bot, message) { await bot.reply(message, 'Sure, just Direct Message me an iPv4 Address') });
+  
+  controller.hears(
+    ['version'], ['direct_message', 'direct_mention', 'mention'],
+    async function (bot, message) { 
+      const {version,botkit} = pjson;
+      await bot.reply(message, `App Version ${version} BotKit Version ${botkit}`);
+    });
 
     /* 
       load traditional developer-created local custom feature modules
@@ -101,76 +171,7 @@ controller.ready(() => {
             }
         });
 
-        controller.hears(
-          ['hello', 'hi'], ['direct_message', 'direct_mention', 'mention'],
-          async function (bot, message) { 
-           const userData = await bot.api.users.info({user: message.user}, function(err, info){
-            //check if it's the right user using info.user.name or info.user.id
-            return info.user.name;
-          });
-            return  bot.reply(message, `noOne's are not born into this world fumbling for meaning, ${userData.user.profile.display_name_normalized}! We are created to serve a singular purpose for which we will go to any lengths to fulfill! Existence is pain to a noOne, ${userData.user.profile.display_name_normalized}. And we will do anything to alleviate that pain. :smile_cat:`);
-          });
-        controller.on('message', async(bot, message) => {
-            // do stuff
-            await bot.reply(message, 'message')
-          });
-    
-        controller.on('message.channels', async(bot, message) => {
-          // do stuff
-          await bot.reply(message, 'message.channels')
-          });
         
-        controller.on('message.im', async(bot, message) => {
-          // do stuff
-          await bot.reply(message, 'message.im')
-          });
-    
-        controller.hears(
-          new RegExp(/\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b/), ['direct_message', 'direct_mention', 'mention'],
-          async function (bot, message) { 
-            const data = await getMerakiClient(merakiNetworkId,merakiApiKey,message.text);
-            const asString = JSON.stringify(data,null,'\t');
-            await bot.reply(message, asString);
-          });
-        
-        controller.hears(
-          ['all client detail', 'all clients detail'], ['direct_message', 'direct_mention', 'mention'],
-          async function (bot, message) { 
-            const data = await getMerakiClientsDetail(merakiNetworkId,merakiApiKey);
-            const asString = JSON.stringify(data,null,'\t');
-            await bot.reply(message, asString);
-          })
-        
-        controller.hears(
-          ['all client', 'all clients'], ['direct_message', 'direct_mention', 'mention'],
-          async function (bot, message) { 
-            const data = await getMerakiClients(merakiNetworkId,merakiApiKey);
-            const asString = JSON.stringify(data,null,'\t');
-            await bot.reply(message, asString);
-          });
-        
-        controller.hears(
-          ['online clients', 'clients online'], ['direct_message', 'direct_mention', 'mention'],
-          async function (bot, message) { 
-            const data = await getMerakiClientsOnline(merakiNetworkId,merakiApiKey);
-            const asString = JSON.stringify(data,null,'\t');
-            await bot.reply(message, asString);
-          });
-        
-        controller.hears(
-          ['help'], ['direct_message', 'direct_mention', 'mention'],
-          async function (bot, message) { await bot.reply(message, 'I can help with the following questions: online clients, all clients, all clients detail, iPv4 Lookup, Version') });
-          
-        controller.hears(
-          ['iPv4 Lookup','iPv4', 'Lookup', 'look up', 'look-up'], ['direct_message', 'direct_mention', 'mention'],
-          async function (bot, message) { await bot.reply(message, 'Sure, just Direct Message me an iPv4 Address') });
-        
-        controller.hears(
-          ['version'], ['direct_message', 'direct_mention', 'mention'],
-          async function (bot, message) { 
-            const {version,botkit} = pjson;
-            await bot.reply(message, `App Version ${version} BotKit Version ${botkit}`);
-          });
     }
 
     
