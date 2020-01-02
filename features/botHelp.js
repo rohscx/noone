8a0b92aa9c705d7c9338a2ac373c944f87637ddd
Skaffen-Amtiskaw <skaffen@Skaffen-Amtiskaws-MacBook-Pro.local>
2020-01-02 13:04:31 -0500

    bug fix

--- features/botHelp.js
+++ features/botHelp.js
@@ -10,11 +10,12 @@ module.exports = function(controller) {
     show all wired clients on the Meraki
     show all wireless clients on the Meraki
     show vpn logs on the Meraki
+    lookup iPv4 Address 192.168.1.17 from Meraki...
     how many wired clients are online are on the Meraki?
     how many wireless clients are online on the Meraki?
     how many clients are online on the Meraki?
     internet connection health on the Meraki Router
-    iPv4 Lookup
+    
     extract mac address i'm giving you...
     extract ip address i'm giving you...
     show your version
