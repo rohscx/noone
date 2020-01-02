d0bbf357dd1615110792ff6bfa822d3f30361a5c
Skaffen-Amtiskaw <skaffen@SkaffenskawsMBP.localdomain>
2020-01-01 19:50:49 -0500

    addting custom ip funtions

--- features/merakiNetworkStatus.js
+++ features/merakiNetworkStatus.js
@@ -1,3 +1,4 @@
+const {ipFromString} = require('nodeutilz')
 // Custom bot libs
 const getMerakiDeviceLossLatency = require('../lib/getMerakiDeviceLossLatency.js');
 
@@ -11,7 +12,9 @@ module.exports = function(controller) {
   controller.hears(
     ['internet connection', 'internet health', 'internet connection'], ['direct_message', 'direct_mention', 'mention'],
     async function (bot, message) { 
-      const data = await getMerakiDeviceLossLatency(merakiNetworkId,merakiApiKey,merakiGatwayRouter,publicTestIp);
+      const userDefinedIp = ipFromString(message.text)[0];
+      const data = await getMerakiDeviceLossLatency(merakiNetworkId,merakiApiKey,merakiGatwayRouter,publicTestIp,userDefinedIp);
+      
       const asString = JSON.stringify(data,null,'\t');
       if (message.type === "direct_mention") {
         await bot.startConversationInThread(message.channel, message.user, message.incoming_message.channelData.ts);
