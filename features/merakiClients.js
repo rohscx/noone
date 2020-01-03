7139dd4bbde8414b7434b3bb6e22726ac1e71403
Skaffen-Amtiskaw <skaffen@SkaffenskawsMBP.localdomain>
2020-01-02 21:49:19 -0500

    debug

--- features/merakiClients.js
+++ features/merakiClients.js
@@ -41,7 +41,9 @@ module.exports = function(controller) {
       const asString = JSON.stringify(data,null,'\t');
       const hyperDenseString = (data) => {
         return data.map(({description,ip,mac,lastSeen,status})=> {
-          return `${description}\n${ip} ${mac}\n${lastSeen} ${status}`
+          const estOffset = 3600000 * -5;
+          const asEpoch = new Date(lastSeen).getDate();
+          return `${description}\t${ip}\t${mac}\n${new Date(asEpoch+estOffset)}\t${status}`
         }).join('\n\n');
       };
       if (isDirectMessage(message.type,["direct_mention","mention"])) {
