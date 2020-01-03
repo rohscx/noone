7d311249b726d96c6c95ddcdaf86064617256c89
Skaffen-Amtiskaw <skaffen@Skaffen-Amtiskaws-MacBook-Pro.local>
2020-01-03 18:17:46 -0500

    new functions

--- features/merakiNetworkStatus.js
+++ features/merakiNetworkStatus.js
@@ -25,4 +25,34 @@ module.exports = function(controller) {
       }      
     });
 
+    controller.hears(
+      ['are there network errors', ], ['direct_message', 'direct_mention', 'mention'],
+      async function (bot, message) { 
+        const userDefinedIp = ipFromString(message.text)[0];
+        const data0 = await getMerakiDeviceLossLatency(merakiNetworkId,merakiApiKey,merakiGatwayRouter,publicTestIp,userDefinedIp);
+        const data1 = await getMerakiLogsVpn(merakiNetworkId,merakiApiKey);
+        const data2 = await getMerakiLogsDhcp(merakiNetworkId,merakiApiKey);
+        const {lossHealthStatus, latencyHeathStatus }= data0
+        const sample =  (data,sampleKey,sampleSize = 3) => {
+          const keys = [];
+          return data.reduce((n,o,i) => {
+            if(i > 0) {
+              if(!keys.includes(o[sampleKey])) n.push(o);
+              return n;
+            } else {
+              keys.push(o[sampleKey]);
+              n.push(o);
+              return n;
+            }
+          },[]).filter((f,i) => i+1 < sampleSize)
+        };
+        const data = [lossHealthStatus,latencyHeathStatus,data1.filter((f) => f.msg.toLowerCase().search('error') !== -1),sample(data2,"description",3)];
+        const asString = JSON.stringify(data,null,'\t');
+        if (isDirectMessage(message.type,["direct_mention","mention"])) {
+          await bot.startConversationInThread(message.channel, message.user, message.incoming_message.channelData.ts);
+          await bot.reply(message, asString);
+        } else {
+          await bot.reply(message, asString);
+        }      
+      });
 }
